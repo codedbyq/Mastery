c229b2aa268608ce3368571da33c193c7541c014
Marquis Bentley <livefreekai@icloud.com>
2020-07-07 15:08:51 -0700

    Make user action creators

---
+++ frontend/src/actions/user_actions.js
@@ -0,0 +1,26 @@
+import * as userAPI from '../util/user_api_util';
+
+const RECEIVE_USERS = 'RECEIVE_USERS';
+const RECEIVE_USER = 'RECEIVE_USER';
+
+// action creator to receive all users
+const receiveUsers = users =>  ({
+    type: RECEIVE_USERS,
+    users
+});
+
+// action creator to receive a single user
+const receiveUser = user => ({
+    type: RECEIVE_USER,
+    user
+});
+
+// thunk action creator to request users and dipatch them to the state
+export const fetchUsers = () => dispatch => (
+    userAPI.getUsers().then(users => dispatch(receiveUsers()))
+);
+
+// thunk action creator to request users and dipatch them to the state
+export const fetchUser = userId => dispatch => (
+    userAPI.getUser(userId).then(user => dispatch(receiveUser(user)))
+);
