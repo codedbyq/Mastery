6de36ea358875f2a6019441236ab7d117ee41a58
chrisoney <christopher.oney@yahoo.com>
2020-07-08 13:41:03 -0700

    Switch to newest key

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 // keys_dev.js
 module.exports = {
-  mongoURI: 'mongodb+srv://dev:9V3r8YBEVJiBIBKI@cluster0.bxg18.mongodb.net/test?retryWrites=true&w=majority',
-  secretOrKey: 'j6iv9CLE73'
+  mongoURI: 'mongodb+srv://dev:C20JVsD7DVvSg6eh@cluster0.bxg18.mongodb.net/test?retryWrites=true&w=majority',
+  secretOrKey: 'j2rl8HAN32'
 }
