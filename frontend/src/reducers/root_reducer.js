59b9925e63c3ae9f6b0caa865871e86f19db0691
chrisoney <christopher.oney@yahoo.com>
2020-07-08 12:19:52 -0700

    Match up with skills reducer change

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -3,6 +3,7 @@ import session from "./session_api_reducer";
 import errors from "./errors_reducer"
 import entities from "./entities_reducer"
 import ui from "./ui_reducer"
+import skills from "./skills_reducer"
 
 const RootReducer = combineReducers({
   entities,
