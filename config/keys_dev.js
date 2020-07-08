1f22166383dfe55d444f3abf53056d7049525b27
John Lam <johnlamll916@gmail.com>
2020-07-08 12:02:19 -0700

    fixed keys file

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 // keys_dev.js
 module.exports = {
-    mongoURI: 'mongodb+srv://dev:ofS8zkSGrQDWK27m@cluster0.bxg18.mongodb.net/test?retryWrites=true&w=majority',
-    secretOrKey: 'x9i7vOKC37'
+  mongoURI: 'mongodb+srv://dev:9V3r8YBEVJiBIBKI@cluster0.bxg18.mongodb.net/test?retryWrites=true&w=majority',
+  secretOrKey: 'j6iv9CLE73'
 }
