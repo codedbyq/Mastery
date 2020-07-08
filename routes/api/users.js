b9d18ca660cd43fc43ae2a79c4d327e912e0e9e9
John Lam <johnlamll916@gmail.com>
2020-07-07 18:27:57 -0700

    heroku ready

--- routes/api/users.js
+++ routes/api/users.js
@@ -49,7 +49,7 @@ router.post('/register', (req, res) => {
                   });
                 });
               })
-              .catch(err => console.log(err));
+              .catch(err => res.json("Error: " + err));
           })
         })
       }
@@ -59,8 +59,6 @@ router.post('/register', (req, res) => {
 
 router.post('/login', (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
-
-  console.log(errors);
 
   if (!isValid) {
     return res.status(400).json(errors);
