1990c9ea0c617ca15547fd0eb3c92b60610a1393
Marquis Bentley <livefreekai@icloud.com>
2020-07-08 18:55:48 -0700

    Format how the user reducer receives the user data

--- frontend/src/reducers/entities/users_reducer.js
+++ frontend/src/reducers/entities/users_reducer.js
@@ -13,7 +13,8 @@ const usersReducer = (state = {}, action) => {
         // receive a single user that we can add to our state as key(user id) - 
         // value(user object) pair
         case RECEIVE_USER:
-            newState[action.user.id] = action.user;
+            debugger
+            newState[action.user.data._id] = action.user.data;
             return newState;
         
         // on login receive the current user and add to our state as key(user id) - 
