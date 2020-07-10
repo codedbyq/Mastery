181a4e28ee8bcd14020a1b8b6632ede264beaaef
gilbu <gilbertbuix57@gmail.com>
2020-07-09 22:35:31 -0700

    fix accordion

--- frontend/src/components/skill/skill.js
+++ frontend/src/components/skill/skill.js
@@ -18,8 +18,8 @@ class Skill extends React.Component {
               integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
               crossorigin="anonymous"
             />
-            <Accordion defaultActiveKey={_id}>
-              <Card>
+            <Accordion >
+              <Card id="card">
                 <Card.Header>
                   <Accordion.Toggle
                     id="skill-accordion"
