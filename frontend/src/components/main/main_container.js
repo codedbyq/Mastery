abf235308c27de1859a2e18d19d5f74d4ae726a5
Marquis Bentley <livefreekai@icloud.com>
2020-07-09 22:31:35 -0700

    Adjust landing page size

--- frontend/src/components/main/main_container.js
+++ frontend/src/components/main/main_container.js
@@ -1,13 +1,15 @@
 import { connect } from 'react-redux';
 import { fetchUsers } from '../../actions/user_actions';
+import { openModal } from "../../actions/modal_actions";
 import MainPage from './main_page.jsx';
 
 const mapStateToProps = state => ({
     users: Object.values(state.entities.users)
 });
 
-const mapDispatchToProps = dispatch => ({
-    fetchUsers: () => dispatch(fetchUsers())
+const mapDispatchToProps = (dispatch) => ({
+  fetchUsers: () => dispatch(fetchUsers()),
+  openModal: (modal) => dispatch(openModal(modal)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
