f44643d4bd672123dcb62d98c53e4f6c10233dc5
Marquis Bentley <livefreekai@icloud.com>
2020-07-09 21:59:38 -0700

    Add text and styling

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -6,13 +6,31 @@ class MainPage extends React.Component {
   render() {
     return (
       <div className="landing-page">
+        <div className='top-session-btns'>
+          <button>Login</button>
+          <button>Sign Up</button>
+        </div>
         <div className="landing-frame">
           <MainClock />
           <h1>Time's Ticking.</h1>
         </div>
-        <div>
-          <p>Study's suggest that it takes 10,000 hours for a human to master a skill.</p>
+        <div className="landing-words">
+          <p className="text-left">
+            Study's suggest that it takes about <span id="bold">10,000</span> hours
+            for a human to truly master any skill.
+          </p>
+          <img
+            className="landing-img"
+            src="https://mastery.s3.us-west-1.amazonaws.com/education.png"
+            />
         </div>
+          <p className='bottom-text'>
+            Mastery gives you the tools necessary to become a mastery at the
+            skill or skills of your choice.
+          </p>
+          <div className='landing-end'>
+            <p>Time's still ticking. Get started today</p>
+          </div>
       </div>
     );
   }
