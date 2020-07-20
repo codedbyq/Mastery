f90d43e308efd61cf884a0f0339e0c2079f02fc5
Marquis Bentley <livefreekai@icloud.com>
2020-07-20 14:05:51 -0700

    Center the logo on the landing page

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -13,7 +13,6 @@ class MainPage extends React.Component {
 
         <div className="top-session-btns">
           <div className="landing-left">
-            <img className='landing-logo' src="https://emilyschromm.com/wp-content/themes/emfit/assets/footer/footer-logo-2x.png" alt='footer-logo'/>
             <button
               className="top-session-btn"
               onClick={() => this.props.openModal("signup")}
@@ -21,6 +20,8 @@ class MainPage extends React.Component {
               Sign Up
             </button>
           </div>
+          
+          <img className='landing-logo' src="https://emilyschromm.com/wp-content/themes/emfit/assets/footer/footer-logo-2x.png" alt='footer-logo'/>
 
           <button
             className="top-session-btn"
@@ -32,40 +33,43 @@ class MainPage extends React.Component {
 
         <div className="landing-frame">
           <MainClock />
-          <h1> Welcome to Mastery</h1>
+          <h1 id='landing-title'> Welcome to Mastery</h1>
           <h1 className='logo-text'>Time's Ticking.</h1>
         </div>
-        <div className="landing-words">
-          <p className="text-left">
-            Study's suggest that it takes about <span id="bold">10,000</span>{" "}
-            hours for a human to truly master any skill.
-          </p>
-          <img
-            className="landing-img"
-            src="https://mastery.s3.us-west-1.amazonaws.com/education.png"
-            alt='landing'
-          />
-        </div>
-        <p className="bottom-text">
-          Mastery gives you the tools necessary to become a mastery at the skill
-          or skills of your choice.
-        </p>
-        <div className="landing-end">
-          <p className="closing-text">Time's still ticking. Get started today.</p>
-          <div className="bottom-session-btns">
-            <button
-              className="bottom-session-btn"
-              onClick={() => this.props.openModal("signup")}
-            >
-              Sign Up
-            </button>
 
-            <button
-              className="bottom-session-btn"
-              onClick={() => this.props.openModal("login")}
-            >
-              Login
-            </button>
+        <div className='second-frame'>
+          <div className="landing-words">
+            <p className="text-left">
+              Study's suggest that it takes about <span id="bold">10,000</span>{" "}
+              hours for a human to truly master any skill.
+            </p>
+            <img
+              className="landing-img"
+              src="https://mastery.s3.us-west-1.amazonaws.com/education.png"
+              alt='landing'
+            />
+          </div>
+          <p className="bottom-text">
+            Mastery gives you the tools necessary to become a mastery at the skill
+            or skills of your choice.
+          </p>
+          <div className="landing-end">
+            <p className="closing-text">Time's still ticking. Get started today.</p>
+            <div className="bottom-session-btns">
+              <button
+                className="bottom-session-btn"
+                onClick={() => this.props.openModal("signup")}
+              >
+                Sign Up
+              </button>
+
+              <button
+                className="bottom-session-btn"
+                onClick={() => this.props.openModal("login")}
+              >
+                Login
+              </button>
+            </div>
           </div>
         </div>
       </div>
