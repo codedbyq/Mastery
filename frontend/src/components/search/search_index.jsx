8c114d4860bda06d698d39fe1f3a4cf739f9b467
Marquis Bentley <livefreekai@icloud.com>
2020-07-20 12:06:11 -0700

    Style the search index component to match the user dashboard

--- frontend/src/components/search/search_index.jsx
+++ frontend/src/components/search/search_index.jsx
@@ -13,40 +13,51 @@ class SearchIndex extends React.Component {
     }
 
     render() {
+        const resultsFound = `There were ${this.props.users.length} results found.`
         const searchResult = this.props.users ? this.props.users.map(user => (
             <li>{user.username}</li> 
         )) : 'No results found... narrow down your search and try again.'
 
+
         return (
             <div className='search-index'>
-                <section className='content-main'>
-                    <ul>
-                        { searchResult }
-                    </ul>
-                </section>
 
-                <section className="content-side">
-                    <UserInfoContainer />
-                    <Modal />
-                    <div className="create-new-btn">
-                        <div>
-                            <button
-                                className="new-skill-btn light-button"
-                                onClick={() => this.props.openModal("create skill")}>
-                            
-                                New Skill
-                            </button>
-                        </div>
-                        <div>
-                            <button 
-                                className="light-button" 
-                                onClick={() => this.props.openModal("createTask")}> 
+                <div className="result-header">
+                    <h1>Search Results</h1>
+                    <p>{resultsFound}</p>
+                </div>
 
-                                New Task 
-                            </button>
+                <div className='results-content'>
+                    <section id='results-index' className='content-main'>
+                        <h1>Users Found</h1>
+                        <ul className='result-list'>
+                            { searchResult }
+                        </ul>
+                    </section>
+
+                    <section className="content-side">
+                        <UserInfoContainer />
+                        <Modal />
+                        <div className="create-new-btn">
+                            <div>
+                                <button
+                                    className="new-skill-btn light-button"
+                                    onClick={() => this.props.openModal("create skill")}>
+                                
+                                    New Skill
+                                </button>
+                            </div>
+                            <div>
+                                <button 
+                                    className="light-button" 
+                                    onClick={() => this.props.openModal("createTask")}> 
+
+                                    New Task 
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                </section>
+                    </section>
+                </div>
             </div>
         )
     }
