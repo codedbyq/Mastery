873353ab4975bdc7e0a73b562e8ccdbb74e7e08e
Marquis Bentley <livefreekai@icloud.com>
2020-07-20 10:51:15 -0700

    Create a search index container and presentational component

---
+++ frontend/src/components/search/search_index.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import UserInfoContainer from '../user/user_info_container';
+import Modal from '../modal/modal';
+
+class SearchIndex extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    componentDidMount() {
+        this.props.fetchUsers();
+    }
+
+    render() {
+        const searchResult = this.props.users ? this.props.users : 'No results found... narrow down your search and try again.'
+
+        return (
+            <div className='search-index'>
+                <section className='content-main'>
+                    <ul>
+                        { searchResult }
+                    </ul>
+                </section>
+
+                <section className="content-side">
+                    <UserInfoContainer />
+                    <Modal />
+                    <div className="create-new-btn">
+                        <div>
+                            <button
+                                className="new-skill-btn light-button"
+                                onClick={() => this.props.openModal("create skill")}>
+                            
+                                New Skill
+                            </button>
+                        </div>
+                        <div>
+                            <button 
+                                className="light-button" 
+                                onClick={() => this.props.openModal("createTask")}> 
+
+                                New Task 
+                            </button>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
--- frontend/src/components/search/search_index_container.js
+++ frontend/src/components/search/search_index_container.js
@@ -3,14 +3,18 @@ import SearchIndex from './search_index';
 import fetchUsers from '../../actions/user_actions';
 
 const mapStateToProps = (state, ownProps) => {
+    const users = Object.values(state.entities.users);
+    const input = ownProps.match.params.input;
+    const filtered = users.filter(user => user.username.toLowerCase().includes(input));
 
     return {
-
+        users = filtered
     }
 };
 
 const mapDispatchToProps = dispatch => ({
-   fetchUsers: () => dispatch(fetchUsers()) 
+   fetchUsers: () => dispatch(fetchUsers()),
+    openModal: (modal) => dispatch(openModal(modal)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SearchIndex);
