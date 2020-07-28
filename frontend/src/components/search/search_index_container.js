7e92dd0ed6ad6252fa0578150ca32ebc60966b70
Marquis Bentley <livefreekai@icloud.com>
2020-07-27 20:05:38 -0700

    Add skills to search results

--- frontend/src/components/search/search_index_container.js
+++ frontend/src/components/search/search_index_container.js
@@ -1,20 +1,27 @@
 import { connect } from 'react-redux';
 import SearchIndex from './search_index';
 import { fetchUsers } from '../../actions/user_actions';
+import { getSkills } from '../../actions/skill_actions';
 import { openModal } from '../../actions/modal_actions';
 
 const mapStateToProps = (state, ownProps) => {
     const users = Object.values(state.entities.users);
+    const skills = Object.values(state.entities.skills);
+
     const input = ownProps.match.params.input;
-    const filtered = users ? users.filter(user => user.username.toLowerCase().includes(input)) : null;
+    const filteredUsers = users ? users.filter(user => user.username.toLowerCase().includes(input)) : null;
+    const filteredSkills = skills ? skills.filter(skill => 
+        skill.title.toLowerCase().includes(input) || skill.category.toLowerCase().includes(input)) : null;
 
     return {
-        users: filtered
+        users: filteredUsers,
+        skills: filteredSkills
     }
 };
 
 const mapDispatchToProps = dispatch => ({
    fetchUsers: () => dispatch(fetchUsers()),
+   getSkills: () => dispatch(getSkills()),
     openModal: (modal) => dispatch(openModal(modal)),
 });
 
